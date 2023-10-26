9aa6ade7cb74caf50dd6c31feab2cb8c1e897528
jorge <jcham1065@incostarija.org>
2023-10-26 19:01:44 -0400

    subiendo carpeta

--- privada/reportes/js/mostrar_inmueble.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-function mostrar(id_inmueble) {
-    var d1, ventanaCalendario;
-    d1=id_inmueble;
-    
-    ventanaCalendario=window.open("ficha_tec_inmuebles1.php?id_inmueble=" +d1 , "calendario","width=600, heigth=550, left=100, top=100,scrollbars=yes,menubars=no, statusbar=NO,status=NO,resizable=YES,location=NO")
-}
--- privada/reportes/js/mostrar_pruebas.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-function mostrar(id_prueba) {
-    var d1, ventanaCalendario;
-    d1=id_prueba;
-    
-    ventanaCalendario=window.open("ficha_tec_pruebas1.php?id_prueba=" +d1 , "calendario","width=600, heigth=550, left=100, top=100,scrollbars=yes,menubars=no, statusbar=NO,status=NO,resizable=YES,location=NO")
-}
